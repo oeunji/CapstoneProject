9ab0ad3913681b8f74a21dc3d6143c4aa405fc82
eunji <oieunxe@gmail.com>
2025-05-09 17:37:56 +0900

    add: 프로젝트 생성

---
+++ CapstoneProject/CapstoneProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapstoneProject
+//
+//  Created by 이은지 on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .magenta
+    }
+
+
+}
+
